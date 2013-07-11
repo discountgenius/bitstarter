564e69475ec5846a698ce2a9c93e439c210120de
Ubuntu <ubuntu@ip-172-31-38-3.us-west-2.compute.internal>
2013-07-11 02:45:54 +0000

    reading from index.html

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var contents = fs.readFileSync("index.html");
+
+var contents_string = contents.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(contents_string);
 });
 
 var port = process.env.PORT || 5000;
